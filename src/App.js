a9cd29c3e9f920928281028a75928c0cb4b48c4d
Luis <machadoluismendes@gmail.com>
2020-06-25 10:21:17 -0300

    test demo

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+        SASS demo
+        </p>
+        <a
+          className="App-link"
+          href="https://sass-lang.com/guide"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn SASS
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
